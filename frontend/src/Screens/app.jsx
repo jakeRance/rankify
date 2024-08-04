89644b35e545d79ef5d3edbcdedba5fa09b7afb5
jakeRance <jake.rance01@gmail.com>
2024-08-03 21:22:20 -0500

    Actually fix api url

--- frontend/src/Screens/app.jsx
+++ frontend/src/Screens/app.jsx
@@ -22,7 +22,7 @@ function app() {
 
   const resetSongPool = async () => {
     try {
-      const response = await axios.post("https://rankify-back.vercel.app/api/reset");
+      const response = await axios.post("https://vercel.com/jakerances-projects/rankify-back/DkiWxxadhhF8w7aXdjAh2Pga4rLB/api/reset");
       console.log(response.data.message);
     } catch (error) {
       console.error('Error resetting song pool:', error);
@@ -47,7 +47,7 @@ function app() {
     try {
       //Reset song_pool so past rounds don't carry over
       resetSongPool()
-      const response = await axios.post("https://rankify-back.vercel.app/api/pairs", objectsList);
+      const response = await axios.post("https://vercel.com/jakerances-projects/rankify-back/DkiWxxadhhF8w7aXdjAh2Pga4rLB/api/pairs", objectsList);
       setData(response.data);
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -56,7 +56,7 @@ function app() {
 
 const sendResults = async () => {
   try {
-      const response = await axios.post("https://rankify-back.vercel.app/api/results", data);
+      const response = await axios.post("https://vercel.com/jakerances-projects/rankify-back/DkiWxxadhhF8w7aXdjAh2Pga4rLB/api/results", data);
       setSongs(response.data.processedData);
   } catch (error) {
       console.error('Error fetching data:', error);
